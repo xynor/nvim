708dccd6bd2347b78163ff551565a61ac2907581
xynor <ace.xinxuwang@gmail.com>
2024-11-22 15:52:04 +0800

    neovide

--- init.lua
+++ init.lua
@@ -1,2 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+if vim.g.neovide then
+    vim.o.guifont = "Hack Nerd Font Mono:h16"
+    vim.g.neovide_input_macos_option_key_is_meta = "only_left"
+end
