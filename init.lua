8462ccf59eea47fc5f4369234c147a20f05e42bf
xynor <ace.xinxuwang@gmail.com>
2024-11-23 17:36:50 +0800

    font & animation

--- init.lua
+++ init.lua
@@ -1,6 +1,11 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 if vim.g.neovide then
-    vim.o.guifont = "Hack Nerd Font Mono:h16"
+    vim.o.guifont = "Hack Nerd Font"
     vim.g.neovide_input_macos_option_key_is_meta = "only_left"
+    vim.g.neovide_position_animation_length = 0
+    vim.g.neovide_cursor_animation_length = 0.00
+    vim.g.neovide_cursor_trail_size = 0
+    vim.g.neovide_cursor_animate_in_insert_mode = false
+    vim.g.neovide_cursor_animate_command_line = false
 end
