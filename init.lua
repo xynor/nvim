60a5287df665622850dba3358f913f9b69987a26
xynor <ace.xinxuwang@gmail.com>
2024-11-25 18:09:58 +0800

    OSX Font Size

--- init.lua
+++ init.lua
@@ -1,7 +1,12 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 if vim.g.neovide then
-    vim.o.guifont = "Hack Nerd Font"
+    local os_name = jit.os
+    if os_name == "OSX" then
+        vim.o.guifont = "Hack Nerd Font:h16"
+    else
+        vim.o.guifont = "Hack Nerd Font:h14"
+    end
     vim.g.neovide_input_macos_option_key_is_meta = "only_left"
     vim.g.neovide_position_animation_length = 0
     vim.g.neovide_cursor_animation_length = 0.00
