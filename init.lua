d8bffbccbbecc645825b9197c032d276c10fdae9
xynor <ace.xinxuwang@gmail.com>
2024-11-29 13:38:14 +0800

    grep_arg

--- init.lua
+++ init.lua
@@ -4,8 +4,12 @@ if vim.g.neovide then
     local os_name = jit.os
     if os_name == "OSX" then
         vim.o.guifont = "Hack Nerd Font:h16"
+        vim.keymap.set("i", "<D-v>", '<ESC>l"+Pli') -- Paste insert mode
+        vim.api.nvim_set_keymap("!", "<D-v>", "<C-R>+", { noremap = true, silent = true })
     else
         vim.o.guifont = "Hack Nerd Font:h14"
+        vim.keymap.set("i", "<M-v>", '<ESC>l"+Pli') -- Paste insert mode
+        vim.api.nvim_set_keymap("!", "<M-v>", "<C-R>+", { noremap = true, silent = true })
     end
     vim.g.neovide_input_macos_option_key_is_meta = "only_left"
     vim.g.neovide_position_animation_length = 0
