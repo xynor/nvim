9e893e794f3f8a79b00cfbb497d3d47ef7a1fb1d
xynor <ace.xinxuwang@gmail.com>
2024-12-26 18:05:57 +0800

    goplements

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,5 +1,23 @@
 return {
     {
+        "maxandron/goplements.nvim",
+        ft = "go",
+        opts = {
+            -- The prefixes prepended to the type names
+            prefix = {
+                interface = "implemented by: ",
+                struct = "implements: ",
+            },
+            -- Whether to display the package name along with the type name (i.e., builtins.error vs error)
+            display_package = true,
+            -- The namespace to use for the extmarks (no real reason to change this except for testing)
+            namespace_name = "goplements",
+            -- The highlight group to use (if you want to change the default colors)
+            -- The default links to DiagnosticHint
+            highlight = "@number",
+        },
+    },
+    {
         "neovim/nvim-lspconfig",
         opts = {
             inlay_hints = {
