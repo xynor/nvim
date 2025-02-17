5aa987113d11b288c6105622ca9f78ca0b6c97d0
xynor <ace.xinxuwang@gmail.com>
2025-02-17 15:50:41 +0800

    vscode

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,3 +7,16 @@
 -- map("n", "<A-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
 -- map("n", "<A-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
 -- map("n", "<A-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
+if vim.g.vscode then
+    local map = vim.keymap.set
+    local vscode = require("vscode")
+    map("n", "gI", "<cmd>lua require('vscode').action('editor.action.goToImplementation')<CR>")
+    map("n", "gr", "<cmd>lua require('vscode').action('editor.action.goToReferences')<CR>")
+    map("n", "]d", "<cmd>lua require('vscode').action('editor.action.marker.nextInFiles')<CR>")
+    map("n", "[d", "<cmd>lua require('vscode').action('editor.action.marker.prevInFiles')<CR>")
+    map("n", "<leader>ca", "<cmd>lua require('vscode').action('editor.action.quickFix')<CR>")
+    map("n", "<leader>e", function()
+        -- vscode.action("workbench.view.explorer")
+        vscode.action("workbench.action.toggleSidebarVisibility")
+    end)
+end
